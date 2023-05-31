10e38ceb3cbe3a70845b0ca551f046f68eec9499
RadityaDito <raditya.ihsan@ui.ac.id>
2023-05-31 17:46:18 +0700

    Clean Backend Code

--- Backend/config/connectToDatabase.js
+++ Backend/config/connectToDatabase.js
@@ -1,16 +1,5 @@
 const { Client } = require("pg");
 
-// const db = new Client({
-//   //Tambahkan parameter yang sesuai untuk melakukan koneksi dengan database
-//   user: "ditoraditya04",
-//   host: "ep-curly-star-451500.ap-southeast-1.aws.neon.tech",
-//   database: "Testing_Proyek",
-//   password: "bCgO5MB0ZoHQ",
-//   port: 5432,
-//   sslmode: "require",
-//   ssl: true,
-// });
-
 const db = new Client({
   //Tambahkan parameter yang sesuai untuk melakukan koneksi dengan database
   user: process.env.DATABASE_USER,
